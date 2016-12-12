3951e1dea075821a948138ed96270a5ddb67fee1
marcpaul <marcpaul@gmx.de>
2016-12-12 23:34:32 +0100

    added first imagemap changed styles added qtip

---
+++ javascripts/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    $("area").qtip({ // Grab all elements with a non-blank data-tooltip attr.
+    content: {
+        text: function(event, api) {
+            // Retrieve content from custom attribute of the $('.selector') elements.
+            return $(this).attr("data-tooltip-content");
+        },
+        title: function(event, api) {
+            // Retrieve content from custom attribute of the $('.selector') elements.
+            return $(this).attr("data-tooltip-title");
+        }
+    },
+    style: {
+        classes: "qtip-dark",
+        tip: {
+            corner: false
+        }
+    },
+    position: {
+        target: 'mouse',
+        adjust: {
+            x: 30
+        }
+    }
+});
+
+
+});
